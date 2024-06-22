7b72e268c05fe72275da7e4b8c30656cfe1469e2
Yong Tang <4312948+teskobif7@users.noreply.github.com>
2024-06-22 20:44:30 +0000

    Add robot_description from publisher

--- joint_state_transformer/transformer.py
+++ joint_state_transformer/transformer.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 
@@ -11,21 +12,34 @@ class TransformerNode(Node):
 
         self.declare_parameter("pose", "~/pose")
         self.declare_parameter("joint_states", "~/joint_states")
+        self.declare_parameter("robot_description", "~/robot_description")
 
         self.pose_ = self.get_parameter("pose").get_parameter_value().string_value
         self.joint_states_ = (
             self.get_parameter("joint_states").get_parameter_value().string_value
         )
+        self.robot_description_ = (
+            self.get_parameter("robot_description").get_parameter_value().string_value
+        )
         self.get_logger().info(
-            "parameters: pose={} joint_states={}".format(
+            "parameters: pose={} joint_states={} robot_description={}".format(
                 self.pose_,
                 self.joint_states_,
+                self.robot_description_,
             )
         )
 
         self.publisher_ = self.create_publisher(JointState, self.joint_states_, 10)
         self.subscription_ = self.create_subscription(
             PoseStamped, self.pose_, self.subscription_callback, 10
+        )
+        self.description_ = self.create_subscription(
+            String,
+            self.robot_description_,
+            self.description_callback,
+            qos_profile=rclpy.qos.QoSProfile(
+                depth=1, durability=rclpy.qos.DurabilityPolicy.TRANSIENT_LOCAL
+            ),
         )
 
     def subscription_callback(self, msg):
@@ -34,6 +48,9 @@ class TransformerNode(Node):
         msg.name = ["demo"]
         msg.position = [0.0]
         self.publisher_.publish(msg)
+
+    def description_callback(self, msg):
+        self.get_logger().info(f"description: data={msg}")
 
 
 def main(args=None):
--- launch/launch.demo.py
+++ launch/launch.demo.py
@@ -1,15 +1,50 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
+import launch
+import launch_ros
+import moveit_configs_utils
 
 
 def generate_launch_description():
-    return LaunchDescription(
+    ros2_control_hardware_type = launch.actions.DeclareLaunchArgument(
+        "ros2_control_hardware_type",
+        default_value="mock_components",
+        description="ROS2 control hardware interface type to use for the launch file -- possible values: [mock_components, isaac]",
+    )
+
+    moveit_config = (
+        moveit_configs_utils.MoveItConfigsBuilder("moveit_resources_panda")
+        .robot_description(
+            file_path="config/panda.urdf.xacro",
+            mappings={
+                "ros2_control_hardware_type": launch.substitutions.LaunchConfiguration(
+                    "ros2_control_hardware_type"
+                )
+            },
+        )
+        .robot_description_semantic(file_path="config/panda.srdf")
+        .trajectory_execution(file_path="config/gripper_moveit_controllers.yaml")
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
+        )
+        .to_moveit_configs()
+    )
+
+    return launch.LaunchDescription(
         [
-            Node(
+            ros2_control_hardware_type,
+            launch_ros.actions.Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                parameters=[moveit_config.robot_description],
+            ),
+            launch_ros.actions.Node(
                 executable="transformer",
                 package="joint_state_transformer",
                 output="screen",
                 parameters=[{}],
+                remappings=[
+                    ("~/robot_description", "/robot_description"),
+                ],
             ),
         ]
     )
