4770052a4b30ebf7e4b22ed13ba27c5f8493ed85
Yong Tang <4312948+teskobif7@users.noreply.github.com>
2024-06-22 20:49:33 +0000

    Update test

--- test/test_transformer.py
+++ test/test_transformer.py
@@ -2,6 +2,7 @@ import pytest
 import launch
 import launch_ros
 import launch_pytest
+import moveit_configs_utils
 
 
 @pytest.fixture
@@ -34,15 +35,49 @@ def proc_sub():
 
 @launch_pytest.fixture
 def launch_description(proc_pub, proc_sub):
+    ros2_control_hardware_type = launch.actions.DeclareLaunchArgument(
+        "ros2_control_hardware_type",
+        default_value="mock_components",
+        description="ROS2 control hardware interface type to use for the launch file -- possible values: [mock_components, isaac]",
+    )
+
+    moveit_config = (
+        moveit_configs_utils.MoveItConfigsBuilder("moveit_resources_panda")
+        .robot_description(
+            file_path="config/panda.urdf.xacro",
+            mappings={
+                "ros2_control_hardware_type": launch.substitutions.LaunchConfiguration(
+                    "ros2_control_hardware_type"
+                )
+            },
+        )
+        .robot_description_semantic(file_path="config/panda.srdf")
+        .trajectory_execution(file_path="config/gripper_moveit_controllers.yaml")
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
+        )
+        .to_moveit_configs()
+    )
+
     return launch.LaunchDescription(
         [
             proc_pub,
             proc_sub,
+            ros2_control_hardware_type,
+            launch_ros.actions.Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                parameters=[moveit_config.robot_description],
+            ),
             launch_ros.actions.Node(
                 executable="transformer",
                 package="joint_state_transformer",
                 output="screen",
                 parameters=[{}],
+                remappings=[
+                    ("~/robot_description", "/robot_description"),
+                ],
             ),
         ]
     )
